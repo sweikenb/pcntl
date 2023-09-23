37d5548cbd3cede1a974e5ed9ecfb99967ded9c0
Simon Schröer <366399+sweikenb@users.noreply.github.com>
2023-09-23 21:28:14 +0200

    Add ProcessQueue feature (#7)

---
+++ src/ProcessQueue.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sweikenb\Library\Pcntl;
+
+use Sweikenb\Library\Pcntl\Api\ChildProcessInterface;
+use Sweikenb\Library\Pcntl\Api\ProcessManagerInterface;
+use Sweikenb\Library\Pcntl\Api\ProcessQueueInterface;
+
+class ProcessQueue implements ProcessQueueInterface
+{
+    private ProcessManagerInterface $processManager;
+    private int $maxThreads;
+    private int $threadCounter = 0;
+
+    public function __construct(
+        int $maxThreads,
+        ?ProcessManagerInterface $processManager = null
+    ) {
+        $this->processManager = $processManager ?? new ProcessManager();
+        $this->maxThreads = max(1, $maxThreads);
+    }
+
+    public function addToQueue(callable $callback): ChildProcessInterface
+    {
+        while ($this->threadCounter >= $this->maxThreads) {
+            $this->processManager->wait(fn() => --$this->threadCounter >= $this->maxThreads);
+        }
+        $this->threadCounter++;
+        return $this->processManager->runProcess($callback);
+    }
+}
